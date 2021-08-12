ed651b6f36f4ba4c81e19d40d657333d5660d98c
nikich5 <enik3.ru@gmail.comsgit config --global user.email enik3.ru@gmail.com>
2021-08-12 22:55:54 +0500

    v 1.1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,18 +2,35 @@ import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    var monthAmount = 0
-    var transferAmount: Int
     while(true) {
-        println("Введите размер перевода в копейках")
-        transferAmount = scanner.nextInt()
+        println("Введите размер перевода в рублях")
+        val transferAmount = scanner.nextInt() * 100
 
-        println("Введите тип перевода")
-        val paymentTypeString = scanner.next()
-        val paymentType = PaymentType.valueOf(paymentTypeString)
-        if (paymentType == PaymentType.MasterCard || paymentType == PaymentType.Maestro) {
-            monthAmount += transferAmount
+        println("Выберите платежную систему: \n1. VK Pay\n2. MasterCard\n3. Maestro\n4. Visa\n5. Mir")
+        val paymentType = when (scanner.nextInt()) {
+            1 -> PaymentType.Vk
+            2 -> PaymentType.MasterCard
+            3 -> PaymentType.Maestro
+            4 -> PaymentType.Visa
+            5 -> PaymentType.Mir
+            else -> continue
         }
+
+        println("Введите сумму предыдущих переводов (в рублях) в этом месяце (по умолчанию - 0)")
+        val monthAmount = scanner.nextInt() * 100
+
+        if (transferAmount < 5_000) {
+            println("Минимальная сумма перевода 50 р.")
+            continue
+        } else if (paymentType != PaymentType.Vk && transferAmount > 15_000_000 || monthAmount > 60_000_000) {
+            println("Максимальная сумма переводов по карте 150000 р. в сутки и 600000 р. в месяц")
+            continue
+        } else if (paymentType == PaymentType.Vk && transferAmount > 1_500_000
+            || paymentType == PaymentType.Vk && monthAmount > 4_000_000) {
+            println("Максимальная сумма переводов через VK Pay - 15 т.р за один раз и 40000 р. в месяц")
+            continue
+        }
+
         println("Комиссия за перевод составит: ${calcOfCommission(transferAmount, monthAmount, paymentType)} копеек")
     }
 }
