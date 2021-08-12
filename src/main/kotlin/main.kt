0ae909baa8efc2e2ab190a328a5c850f192b72e5
nikich5 <enik3.ru@gmail.comsgit config --global user.email enik3.ru@gmail.com>
2021-08-12 15:41:21 +0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,44 @@
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    var monthAmount = 0
+    var transferAmount: Int
+    while(true) {
+        println("Введите размер перевода в копейках")
+        transferAmount = scanner.nextInt()
+        monthAmount += transferAmount
+        println("Введите тип перевода")
+        val paymentTypeString = scanner.next()
+        val paymentType = PaymentType.valueOf(paymentTypeString)
+        println("Комиссия за перевод составит: ${calcOfCommission(transferAmount, monthAmount, paymentType)} копеек")
+    }
+}
+
+fun calcOfCommission(transferAmount: Int,
+                     monthAmount: Int = 0,
+                     paymentType:PaymentType = PaymentType.Vk): Int {
+    when(paymentType) {
+        PaymentType.Maestro, PaymentType.MasterCard -> {
+            return if(transferAmount >= 30_000 && monthAmount <= 7_500_000) {
+                0
+            } else {
+                ((transferAmount * 0.006) + 2_000).toInt()
+            }
+        }
+        
+        PaymentType.Visa, PaymentType.Mir -> {
+            return if((transferAmount * 0.0075) < 3_500) {
+                3_500
+            } else {
+                (transferAmount * 0.0075).toInt()
+            }
+        }
+
+        PaymentType.Vk -> return 0
+    }
+}
+
+enum class PaymentType {
+    Vk, MasterCard, Maestro, Visa, Mir
+}
